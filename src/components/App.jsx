0d24e236afa202fdff0b564b4e4de54057c0fbcb
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-20 19:22:56 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,6 @@ export class App extends Component {
       // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -95,7 +94,6 @@ export class App extends Component {
                 handleChange={this.handleChange}
               />
               <ContactsList
-                // searchValue={filter}
                 contacts={this.filterContacts()} //how it fires???
                 removeContact={this.removeContact}
               />
